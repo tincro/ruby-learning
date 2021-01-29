11f1cbbbfed6678a688728a0a1320e4409adea84
Austin Cronin <austin.cronin3d@gmail.com>
2021-01-29 16:51:14 -0500

    full game

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -73,72 +73,89 @@ class GameBoard
     end
 end
 
-def check_for_winner(player, board, winner)
-    # if either player has one of the matching patterns
-    # that player is announced the winner otherwise keep playing
-    for pattern in board.winning_patterns do
-        connected = 0
-        for space in player.spaces do
-            if pattern.include?(space)
-                connected += 1
-            end
-        end
 
-        if connected == 3
-            puts "#{player.name} wins!"
-            winner = true
-        end 
-    end
-end
 
-def validate_choice(player, board)
-    choice = player.claim
-    if board.slots(choice).is_a? Integer
-        mark = player.piece
-        return board.set_slots(choice, mark)
-    else
-        puts "Try another available space:"
-        validate_choice(player, board)
-    end
-end
+class Game 
+    MAX_TURNS = 5
+    @game_over = false
     
-
-MAX_TURNS = 5
-def main(p1, p2, board)
-    turns = 1
-    winner = false
-    while !winner do
-        board.display_board
-
-        validate_choice(p1, board)
-        board.set_board
-        board.display_board
-
-        check_for_winner(p1, board, winner)
-
-        validate_choice(p2, board)
-        board.set_board
-        board.display_board
-
-        check_for_winner(p2, board, winner)
-        turns += 1
-        if turns == MAX_TURNS
-            puts "Tie game. Thanks for playing!"
-            puts "Play again? y/n"
-            again = gets.chomp
-            if again == "y"
-                board = GameBoard.new
-                main(p1, p2, board)
-            else
-                break
+    def main(p1, p2, board)
+        turns = 1
+        while !@game_over do
+            
+            board.display_board
+            validate_choice(p1, board)
+            board.set_board
+            
+            check_for_winner(p1, board, @game_over)
+            
+            board.display_board
+            validate_choice(p2, board)
+            board.set_board
+    
+            check_for_winner(p2, board, @game_over)
+            turns += 1
+            if turns == Game::MAX_TURNS
+                puts "Tie game. Thanks for playing!"
+                puts "Play again? y/n"
+                again = gets.chomp
+                if again == "y"
+                    reset(p1, p2)
+                else
+                    break
+                end
             end
         end
     end
+
+    def check_for_winner(player, board, winner)
+        # if either player has one of the matching patterns
+        # that player is announced the winner otherwise keep playing
+        for pattern in board.winning_patterns do
+            connected = 0
+            for space in player.spaces do
+                p pattern
+                p space
+                p player.spaces 
+                if pattern.include?(space)
+                    connected += 1
+                end
+            end
+    
+            if connected == 3
+                puts "#{player.name} wins!"
+                puts "Good game!"
+                @game_over = true
+                break
+            end 
+        end
+    end
+
+    def validate_choice(player, board)
+        choice = player.claim
+        if board.slots(choice).is_a? Integer
+            mark = player.piece
+            return board.set_slots(choice, mark)
+        else
+            puts "Try another available space:"
+            validate_choice(player, board)
+        end
+    end
+
+    def reset(player_1, player_2)
+        p1 = Player.new(player_1.name, player_1.piece)
+        p2 = Player.new(player_2.name, player_2.piece)
+        board = GameBoard.new
+        self.main(p1, p2, board)
+    end
+
 end
+
 
 
 p1 = Player.new('Austin', 'A')
 p2 = Player.new('Zach', 'Z')
-game = GameBoard.new
+board = GameBoard.new
+game = Game.new
 
-main(p1, p2, game)
+game.main(p1, p2, board)
